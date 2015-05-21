3e2243b616f7d12cd7f19c2cd32d45460492e105
educriado <ecriado95@gmail.com>
2015-05-22 00:19:15 +0200

    Ya está el script segundo.

--- script2.sh
+++ script2.sh
@@ -3,13 +3,23 @@
 # Pablo Hernandez - 616923
 # Extiende las capacidades de un grupo volumen (parametro) con las particiones (parametros)
 # usar lvextend y luego agrandar el filesystem
+direccion="192.168.56.2"
 
-if [ #? -lt 2]
+if [ "$#" -lt 2 ]
 then
+	echo "Error en el uso de parametros."
 	echo "Uso: $0 grupo {particion}"
 	exit 1
 fi
 
 grupovolum=$1
 
+for arg in "$@"
+do
+	if [ "$arg" != "$0" ] && [ "$arg" != "$1" ]
+	then
+		#Añadimos las particiones al grupo indicado
+		ssh -n user@$direccion sudo vgextend "$1" "$arg"	
+	fi
+done
 
