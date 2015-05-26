2ff551acd0747c39bc4bf1a23ba73772a830a610
educriado <ecriado95@gmail.com>
2015-05-26 12:30:06 +0200

    Los puertos y conexiones con formato.

--- practica6/comprobar.sh
+++ practica6/comprobar.sh
@@ -1,29 +1,64 @@
+#!/bin/bash
+#Eduardo Criado - 662844
+#Pablo Hernandez - 616923
+#Pequeños programas que extraen informacion de monitorizacion de
+#la otra maquina.
+#Una de las dos maquinas mantienen los logs de las dos maquinas (/var/log/monitorizacion)
+
+direccion="192.168.56.2"
+
 #Saber si la maquina está conectada
 ping -c1 www.google.es > /dev/null
 if [ $? -eq 0 ]
 then
-echo La maquina esta conectada
+	echo La maquina esta conectada
 else
-echo La maquina no esta conectada
+	echo La maquina no esta conectada
 fi
+
 #Número de usuarios y carga de procesador
 uptime | tr -s ' ' | cut -d' ' -f7,8,11
+
 #Memoria ram libre y ocupada
 cuenta=0
-free | tr -s ' ' | cut -d' ' -f2,3,4 | (
+free | tr -s ' ' | cut -d' ' -f2,3,4 | 
 while read info1 info2 info3
 do
 	if [ $cuenta -eq 0 ]
 	then
-	echo total	usado	libre
+		echo total	usado	libre
 	elif [ $cuenta -eq 1 ]
 	then
-	echo $info1	$info2	$info3
+		echo $info1	$info2	$info3
 	fi
 	cuenta=$(($cuenta+1))
 done
-)
-#Espacio ocupado y espacio libre
-df
-#Puertos y conexiones abiertos
-#netstat
+
+#Espacio ocupado y espacio libre, en MB en este caso
+df -m | tr -s ' ' | cut -d ' ' -f1,3,4
+#Numero de puertos y conexiones abiertos
+#Las conexiones aparecen despues del primer Active y los puertos despues
+#del segundo.
+active=0
+puertos=0
+echo "Conexiones activas:"
+netstat | 
+while read proto recv send local addr faddr state
+do
+	if [ "$proto" = "Active" ]
+	then 
+		active=$(($active + 1))
+	elif [ "$proto" != "Proto" ]
+	then
+		if [ "$active" -eq 1 ]
+		then
+			#Tenemos conexiones
+			echo "$proto $recv $send $addr $faddr $state"
+		else
+			#Tenemos puertos
+			puertos=$(($puertos + 1))
+		fi
+	fi
+done
+echo "Tenemos $puertos puertos."
+
