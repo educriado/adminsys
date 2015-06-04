f3827672e3e52e2b291d1d109774e848216f3360
educriado <ecriado95@gmail.com>
2015-06-05 00:11:06 +0200

    Script para cambiar las direcciones de /etc/host u otro fichero de hosts. No se modifica el archivo, sino que se sacan los cambios por pantalla

---
+++ ejercicios shell/maquinashost.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Cambiar la direccion de ciertas maquinas en /etc/hosts de forma que el ultimo numero
+#de la direccion sea igual al sufijo del nombre + 40.
+#-------------------------------------------------------------------------------------------
+#  Ejemplo:
+#	212.128.4.213 gamma03.escet.urjc.es gamma0 -> 192.168.0.43 gamma03.escet.urjc.es gamma03
+#
+#-------------------------------------------------------------------------------------------
+
+if [ $# -ne 1 ]; then
+	echo "Uso: $0 fichero_host"
+fi
+
+while read ip dominio host 
+do
+	if [ echo "$host" | egrep 'gamma' ] > /dev/null
+	then
+		numero=$(echo "$host" | sed -r 's/.*(..)/\1/')
+		numero=$((40 + $numero))
+		nuevaip=192.168.0."$numero"
+		echo "$nuevaip $dominio $host"
+	fi
+done < $1
