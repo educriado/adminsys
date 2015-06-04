1da9986510d15ca489c4e586b1105df022f18173
educriado <ecriado95@gmail.com>
2015-06-04 20:34:48 +0200

    Hacia falta utilizar el sort, porque uniq solo tiene en cuenta las lineas adyacentes

--- ejercicios shell/nombresapes.sh
+++ ejercicios shell/nombresapes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Junta los nombres y apellidos y sin repetir quedate con un nombre e indica el numero de repetidos
 
-paste -d ' ' $1 $2 | sort | uniq -c -f1
+paste -d ' ' $1 $2 |  sort -k 2 | uniq -c -f1
